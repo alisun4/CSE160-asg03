e7395ee3895e98a5069b16fb775736f45d036a11
alisunnyday <calpurniahepburn@gmail.com>
2024-05-14 16:44:51 -0700

    camera funcs done working on walls and mouse rotate 5/14

--- src/newWorld.js
+++ src/newWorld.js
@@ -303,28 +303,66 @@ function updateAnimationAngles() {
 
 }
 
-var g_eye = [0,0,3];
-var g_at = [0,0,-100];
-var g_up = [0,1,0];
-// var g_camera = new Camera();
+var g_camera = new Camera();
 
 function keydown(ev) {
-    if (ev.keyCode == 65) {     // D
-        g_eye[0] -= 0.2;
+    if (ev.keyCode == 65) {     // A
+        g_camera.left();
     }
-    else if (ev.keyCode == 68) {    // A
-        g_eye[0] += 0.2;
+    else if (ev.keyCode == 68) {    // D
+        g_camera.right();
     }
     else if (ev.keyCode == 83) {  // S
-        g_eye[2] += 0.2;
+        g_camera.back();
     }
+    else if (ev.keyCode == 87) {  // W
+        g_camera.forward();
+    }
+    else if (ev.keyCode == 81) {    // Q
+        g_camera.panLeft();
+    }
+    else if (ev.keyCode == 69) {    // E
+        g_camera.panRight();
+    }
+
+    else if(ev.keyCode == 82) { // R
+        g_camera.panUp();
+    } 
     
-    else if (ev.keyCode == 87) {
-        g_eye[2] -= 0.2;  // W
+    else if(ev.keyCode == 70) { // F
+        g_camera.panDown();
     }
 
     renderAllShapes();
     console.log(ev.keyCode);
+}
+
+var g_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+];
+    
+
+function drawMap() {
+    for (x = 0; x < 16; x++) {
+        for (y = 0; y < 16; y++) {
+            if (x < 1 || x == 31 || y == 0 || y ==31) {
+                var wall = new Cube();
+                wall.color = [0.8, 1.0, 1.0, 1.0];
+                wall.textureNum = -2;
+                wall.matrix.translate(0, -0.75, 0);
+                wall.matrix.scale(0.3, 0.3, 0.3);
+                wall.matrix.translate(x-8, 0, y-8);
+                wall.renderfast();
+            }
+        }
+    }
 }
 
 function renderAllShapes() {
@@ -335,7 +373,10 @@ function renderAllShapes() {
     gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
 
     var viewMat = new Matrix4();
-    viewMat.setLookAt(g_eye[0], g_eye[1], g_eye[2], g_at[0], g_at[1], g_at[2], g_up[0], g_up[1], g_up[2]);
+    viewMat.setLookAt(
+        g_camera.eye.elements[0], g_camera.eye.elements[1], g_camera.eye.elements[2],
+        g_camera.at.elements[0],  g_camera.at.elements[1],  g_camera.at.elements[2],
+        g_camera.up.elements[0],  g_camera.up.elements[1],  g_camera.up.elements[2]);
     gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
 
     var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0); 
@@ -348,7 +389,7 @@ function renderAllShapes() {
 
     // Colors
     var BLACK = [0.230, 0.182, 0.182, 1.0];
-    var BROWN = [0.610, 0.603, 0.531, 1.0];
+    // var BROWN = [0.610, 0.603, 0.531, 1.0];
     var PINK = [0.830, 0.681, 0.681, 1.0];
     var WHITE = [1.0, 1.0, 1.0, 1.0];
 
@@ -369,14 +410,10 @@ function renderAllShapes() {
     floor.matrix.translate(-0.5, 0.0, -0.5);
     floor.render();
 
-    // drawMap();
-
-
     // Bunny head
 
     var head = new Cube();
-    head.color = BROWN;
-    head.textureNum = 0;
+    head.color = WHITE;
     head.matrix.scale(0.45, 0.4, 0.4);
     head.matrix.translate(0, 0.4, -2.125);
     head.matrix.translate(-0.5, -0.5, 0);
@@ -398,14 +435,14 @@ function renderAllShapes() {
     
     // Ears
     var ear1 = new Cube();
-    ear1.color = BROWN;
+    ear1.color = WHITE;
     ear1.matrix.scale(0.11, 0.6, 0.1);
     ear1.matrix.translate(-1.5, 0.25, -5.75);
     ear1.matrix.rotate(-20, 1, 45, 0);
     ear1.render();
 
     var ear2 = new Cube();
-    ear2.color = BROWN;
+    ear2.color = WHITE;
     ear2.matrix.scale(0.11, 0.6, 0.1);
     ear2.matrix.translate(0.5, 0.25, -5.75);
     ear2.matrix.rotate(20, 1, 45, 0);
@@ -421,8 +458,7 @@ function renderAllShapes() {
     // Body
 
     var body = new Cube();
-    body.color = BROWN;
-    body.textureNum = 1;
+    body.color = WHITE;
     body.matrix.rotate(12, 1, 0, 0);
     body.matrix.scale(0.5, 0.4, 0.8);
     body.matrix.translate(-0.5, -1, -0.8);
@@ -430,14 +466,14 @@ function renderAllShapes() {
 
     // Legs
     var frontlegL = new Cube();
-    frontlegL.color = BROWN;
+    frontlegL.color = WHITE;
     frontlegL.matrix.rotate(12, 1, 0, 0);
     frontlegL.matrix.scale(0.15, 0.5, 0.13);
     frontlegL.matrix.translate(-2, -1.5, -5); 
     frontlegL.render();
 
     var frontlegR = new Cube();
-    frontlegR.color = BROWN;
+    frontlegR.color = WHITE;
     frontlegR.matrix.rotate(12, 1, 0, 0);
     frontlegR.matrix.scale(0.15, 0.5, 0.13);
     frontlegR.matrix.translate(1, -1.5, -5);
@@ -445,7 +481,7 @@ function renderAllShapes() {
 
     // Back haunches
     var haunchL = new Cube();
-    haunchL.color = BROWN;
+    haunchL.color = WHITE;
     haunchL.matrix.rotate(12, 1, 0, 0);
     haunchL.matrix.rotate(g_leftAngle, 1, 0, 0);
     haunchL.matrix.rotate(0, 1, 0, 0);
@@ -455,7 +491,7 @@ function renderAllShapes() {
     haunchL.render();
 
     var haunchR = new Cube();
-    haunchR.color = BROWN;
+    haunchR.color = WHITE;
     haunchR.matrix.rotate(12, 1, 0, 0);
     haunchR.matrix.rotate(g_rightAngle, 1, 0, 0);
     haunchR.matrix.translate(0.15, -0.56, -0.15);
@@ -466,25 +502,20 @@ function renderAllShapes() {
     // Back legs
 
     var backlegL = new Cube();
-    backlegL.color = BROWN;
-    // backlegL.matrix = haunchL.matrix;
+    backlegL.color = WHITE;
     backlegL.matrix = leftCoords;
-    // backlegL.matrix.rotate(-90, 1, 0, 0);
     backlegL.matrix.translate(0, 0, 0.3);
     backlegL.matrix.rotate(280 - g_leftFootAngle, 1, 0, 0);
     backlegL.matrix.translate(0, 0, -0.075 / 2);
-    // backlegL.matrix.translate(0, 0.001, 0.5);
     backlegL.matrix.scale(0.15, 0.5, 0.075);
     backlegL.render();
 
     var backlegR = new Cube();
-    backlegR.color = BROWN;
-    // backlegR.matrix = haunchR.matrix;
+    backlegR.color = WHITE;
     backlegR.matrix = rightCoords;
     backlegR.matrix.translate(0, 0, 0.3);
     backlegR.matrix.rotate(280 - g_rightFootAngle, 1, 0, 0);
     backlegR.matrix.translate(0, 0, -0.075 / 2);
-    // backlegL.matrix.translate(0, 0.001, 0.5);
     backlegR.matrix.scale(0.15, 0.5, 0.075);
     backlegR.render();
 
